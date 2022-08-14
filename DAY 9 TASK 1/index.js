9cd9b8644d5efd9859fa50561a9ffafab8375b46
MuditArya3 <110027831+MuditArya3@users.noreply.github.com>
2022-08-14 16:33:38 +0530

    day 9 task1.2

--- DAY 9 TASK 1/index.js
+++ DAY 9 TASK 1/index.js
@@ -1,11 +1,12 @@
 function printvalue()
 {
-    var name=document.form1.fullname.value;
-    var pwd=document.form1.password.value;
+    var name=document.getElementById("exampleInputEmail1").value;
+    var pwd=document.getElementById("exampleInputPassword1").value;
     alert("welcome : " +name+" / password="+pwd);
 }
 
 function commentform()
 {
-    document.getElementById("hello").innerText="HEY THERE!";
+    // --document.getElementById("hello").="HEY THERE!";
+    alert("Hello & Welcome to Grocify : The online shopping app");
 }
