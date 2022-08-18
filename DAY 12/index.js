1418671b1582f7607efa832207de91862f2094d2
MuditArya3 <110027831+MuditArya3@users.noreply.github.com>
2022-08-18 16:24:08 +0530

    day 12 task1

---
+++ DAY 12/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        document.write("Hurray! You get 50% off!!!");
+    })
+});
+// function sum(...input){
+//     let sum=0;
+//     for(let i of input){
+//         sum+=1;
+//     }
+//     return sum;
+// }
+
+$(document).ready(function(){
+    $('#btn1').click(function(){
+        $('imag').attr("width","100");
+    });
+});
